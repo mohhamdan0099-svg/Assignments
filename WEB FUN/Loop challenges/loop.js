fdd803b9ed522c1c9bbccac7b64140ce1ac6f0e5
Mohammad Hamdan <mohhamdan0099@gmail.com>
2026-09-07 15:32:40 +0300

    loop chalenges

---
+++ WEB FUN/Loop challenges/loop.js
@@ -0,0 +1,35 @@
+for ( var i = 0;  i < 20;i++)  {  // question 1
+    if (i%2!==0) {
+    console.log(i);
+    
+        }
+    }
+
+
+
+for (var i = 100; i >= 0; i--) {      // question 2
+    if (i%3==0) {
+        console.log(i)
+        }
+    
+    }
+
+for(var i=4;i>-4;i-=1.5){            // question 3
+    console.log(i)
+}    
+
+
+var sum=0
+for (var i=0;i<=100;i++) {          //// question 4
+    sum=sum+i;
+
+}
+console.log(sum)
+
+var mult=1
+for (var i=1;i<=12;i++) {         // question 5
+    mult=mult*i;
+
+}
+console.log(mult)
+
