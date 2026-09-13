cd5cfbc4613f3c6f2a69c2b62fc74b48dbfe2a84
Mohammad Hamdan <mohhamdan0099@gmail.com>
2026-09-13 17:47:13 +0300

    dojo

--- WEB FUN/javaScript/DojoWeather/script.js
+++ WEB FUN/javaScript/DojoWeather/script.js
@@ -4,25 +4,28 @@ function hideit(){
 }
 
 function custom(){
-    alert("Loading weather report..");
+    alert();
 }
 
 function changeDegree(){
-    var hotDegree1=document.querySelector(".hot-degreeone")
-    hotDegree1.innerText="75"
-}
+    var hotDegree1=document.querySelector(".hot-degreeone");
+    var hotDegree2=document.querySelector(".hot-degreetwo");
+    var hotDegree3=document.querySelector(".hot-degreethree");
+    var hotDegree4=document.querySelector(".hot-degreefour");
 
-function changeDegree(){
-    var hotDegree2=document.querySelector(".hot-degreetwo")
-    hotDegree2.innerText="80"
-}
+    hotDegree1.innerText="75°";
+    hotDegree2.innerText="80°";
+    hotDegree3.innerText="69°";
+    hotDegree4.innerText="78°";
 
-function changeDegree(){
-    var hotDegree3=document.querySelector(".hot-degreethree")
-    hotDegree3.innerText="69"
-}
+    var coldDegree1=document.querySelector(".cold-degreeone");
+    var coldDegree2=document.querySelector(".cold-degreetwo");
+    var coldDegree3=document.querySelector(".cold-degreethree");
+    var coldDegree4=document.querySelector(".cold-degreefour");
 
-function changeDegree(){
-    var hotDegree4=document.querySelector(".hot-degreethree")
-    hotDegree4.innerText="69"
+    coldDegree1.innerText="65°";
+    coldDegree2.innerText="66°";
+    coldDegree3.innerText="61°";
+    coldDegree4.innerText="70°";
+
 }
