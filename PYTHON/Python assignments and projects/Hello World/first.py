6be881064c010839cd16c60fd848f8ff5b074852
Mohammad Hamdan <mohhamdan0099@gmail.com>
2026-09-24 13:36:48 +0300

    Hello world

---
+++ PYTHON/Python assignments and projects/Hello World/first.py
@@ -0,0 +1,8 @@
+print("Hello World")
+name = "Noelle"
+print(name)
+name = 42
+print(name)
+fav_food1= "sushi"
+fav_food2= "pizza"
+print("I Love to eat " + fav_food1 + " and "+ fav_food2 )
--- personal portfolio/script.js
+++ personal portfolio/script.js
@@ -1,12 +1,12 @@
 // Step 1: Select the root <html> element
 const htmlElement = document.documentElement;
-
 // Step 2: Check if the user previously saved a theme preference
 const savedTheme = localStorage.getItem('userTheme');
 
 if (savedTheme) {
   htmlElement.setAttribute('data-theme', savedTheme);
 }
+
 
 // Step 3: Function called directly by onclick="handleThemeClick()" in HTML
 function handleThemeClick() {
